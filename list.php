db7a3f440e1a6975afc098bdd849c23ed41aec21
a1600540 <a1600540@myy.haaga-helia.fi>
2019-03-07 08:26:24 +0200

    Modifications on list.php

--- list.php
+++ list.php
@@ -1,13 +1,13 @@
-<? php
-session_start();
-if($_SESSION['username'] == 'username') {
-	include "connect.php";
+<?php session_start();
+if($_SESSION['username'] == 'username')
+ {include "connect.php";
 print("<html>");
 $conn = connect_db();
 $sql = "SELECT * FROM Addresses";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {// output data of each row
-while($row = mysqli_fetch_assoc($result)) {echo $row["id"] . " ";
+while($row = mysqli_fetch_assoc($result))
+ {echo $row["id"] . " ";
 echo $row["name"] . " ";
 echo $row["street_address"] . " ";
 echo $row["zip"] . " ";
@@ -15,14 +15,14 @@ echo $row["city"] . " ";
 echo $row["state"] . " ";
 echo $row["country"] . " ";
 echo "<a href=\"delete.php?id=" . $row["id"] . "\">DELETE</a>";
-print("<br>");}
-} else {echo "0 results";
+print("<br>");
+}} else {echo "0 results";
 }print("<br><br>");
 print("Insert new");
 print("<form action=\"save.php\" method=\"post\">");
 print("<br>");
 print("Name: ");
-print("<inputtype=\"text\" name=\"name\">");
+print("<input type=\"text\" name=\"name\">");
 print("<br>");
 print("Street address: ");
 print("<input type=\"text\" name=\"street_address\">");
@@ -44,6 +44,7 @@ print("<input type=\"submit\" name=\"submit\" value=\"Save\" >");
 print("</form>");
 print("</html>");
 mysqli_close($conn);
-} else {print("No access.");
+} else {
+print ("No access.");
 }
 ?>
